daaeb74036e12fb81bd6119bccb6e49be5b760a2
alopez99 <adrianlopezescoriza@gmail.com>
2022-04-23 12:04:42 +0200

    Solve Task 7-8

--- templates/lqr_tuning.m
+++ templates/lqr_tuning.m
@@ -7,5 +7,33 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [tuning_struct, i_opt] = lqr_tuning(x0,Q,params)
-    % YOUR CODE HERE
+    i = 1;
+    i_opt = nan;
+    J_opt = inf;
+
+    for q=Q
+        ctrl = LQR(diag(q),eye(params.model.nu),params);
+        [Xt,Ut] = simulate(x0, ctrl, params);
+        [s_max, y_max, u_max, J_u, df_max, vf_max, traj_feas]... 
+            = traj_constraints(Xt,Ut,params);
+
+        if J_u < J_opt && traj_feas
+            i_opt = i;
+            J_opt = J_u;
+        end
+        tuning_struct(i,:) = struct(...
+            "InitialCondition", x0,...
+            "Qdiag", q,...
+            "MaxAbsPositionXZ", s_max,...
+            "MaxAbsPositionY", y_max,...
+            "MaxAbsThrust", u_max,...
+            "InputCost", J_u,...
+            "MaxFinalPosDiff", df_max,...
+            "MaxFinalVelDiff", vf_max,...
+            "TrajFeasible", traj_feas);
+
+        i = i+1;
+    end
+
+    
 end
--- templates/traj_constraints.m
+++ templates/traj_constraints.m
@@ -7,6 +7,48 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [s_max, y_max, u_max, J_u, df_max, vf_max, traj_feas] = traj_constraints(x,u,params)
-    % YOUR CODE HERE
+    xt = x(1,:);
+    yt = x(2,:);
+    zt = x(3,:);
+
+    x_max = max(abs(xt));
+    z_max = max(abs(zt));
+    y_max = max(abs(yt));
+    u_max = max(max(abs(u)));
+    s_max = max(x_max, z_max);
+
+    J_u = 0;
+    for ut = u
+        J_u = J_u + norm(ut)^2;
+    end
+
+    df_max = norm([xt(end),yt(end),zt(end)]);
+    vf_max = norm([x(4,end),x(5,end),x(6,end)]);
+
+    traj_feas = true;
+    if s_max > params.constraints.MaxAbsPositionXZ 
+        traj_feas = false;
+        return;
+    end
+
+    if y_max > params.constraints.MaxAbsPositionY
+        traj_feas = false;
+        return;
+    end
+
+    if u_max > params.constraints.MaxAbsThrust
+        traj_feas = false;
+        return;
+    end
+
+    if df_max > params.constraints.MaxFinalPosDiff
+        traj_feas = false;
+        return;
+    end
+
+    if vf_max > params.constraints.MaxFinalVelDiff
+        traj_feas = false;
+        return;
+    end
 end
 
