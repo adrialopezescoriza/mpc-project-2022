71097e22d7472abd8c41f171121465fda1cbfe2d
alopez99 <adrianlopezescoriza@gmail.com>
2022-04-23 20:53:22 +0200

    Solve up to task 20

--- templates/MPC_TS.m
+++ templates/MPC_TS.m
@@ -13,7 +13,34 @@ classdef MPC_TS
 
     methods
         function obj = MPC_TS(Q,R,N,H,h,params)
-            % YOUR CODE HERE
+            nu = params.model.nu;
+            nx = params.model.nx;
+
+            % define optimization variables
+            U = sdpvar(repmat(nu,1,N),ones(1,N),'full');
+            X0 = sdpvar(nx,1,'full');
+
+            % define constraints
+            H_x = params.constraints.StateMatrix;
+            H_u = params.constraints.InputMatrix;
+            h_x = params.constraints.StateRHS;
+            h_u = params.constraints.InputRHS;
+
+            [~,P_lqr] = dlqr(params.model.A, params.model.B, Q, R);
+
+            constraints = [];
+            objective = 0;
+            x = X0;
+            for i=1:N
+                objective = objective + traj_cost(x,U{i},Q,R);
+                constraints = [constraints, H_x*x <= h_x, H_u*U{i} <= h_u];
+                x = params.model.A*x + params.model.B*U{i};        
+            end
+
+            objective = objective + x'*P_lqr*x;
+            % Belongs to feasible set
+            constraints = [constraints, H*x <= h];
+
             opts = sdpsettings('verbose',1,'solver','quadprog');
             obj.yalmip_optimizer = optimizer(constraints,objective,opts,X0,{U{1} objective});
         end
